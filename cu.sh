423210ddd64a32de0dfc552b22c0d0d00c14631d
MaxT2 <tristen@payne.org>
2021-12-08 02:24:56 +0000

    updated

--- cu.sh
+++ cu.sh
@@ -1,2 +1,10 @@
+
+# kill all running putty processes to free the serial port
+#killall putty
+
 arduino-cli compile -b arduino:avr:nano
 arduino-cli upload -b arduino:avr:nano:cpu=atmega328old -p /dev/ttyUSB0 -t -v
+
+# start putty
+# & causes it to run in the background so it doesn't block the terminal
+#sudo putty /dev/ttyUSB0 -serial -sercfg 9600,8,n,1,N
--- window-lights.ino
+++ window-lights.ino
@@ -1,3 +1,4 @@
+
 #include <bitswap.h>
 #include <chipsets.h>
 #include <color.h>
@@ -31,8 +32,8 @@
 // How many leds in your strip?
 
 #define LED_TYPE WS2811
-#define COLOR_ORDER GRB
-#define NUM_LEDS 100
+#define COLOR_ORDER RGB
+#define NUM_LEDS 150
 #define DATA_PIN 4
 //#define NUM_STRIPS 1
 //For led chips like Neopixels, which have a data line, ground, and power, you just
@@ -50,11 +51,11 @@
 //CRGB strip6[NUM_LEDS];
 //CRGB strip7[NUM_LEDS];
 CRGB strips[NUM_LEDS];
-int brightness = 60;
+int brightness = 150;
+int brightnessSpeed = 10;
 int saturation = 255;
 int x = 0;
 int xSpeed = 1;
-int pattern = 1;
 
 CHSV red = CHSV(0, saturation, brightness);
 CHSV orange = CHSV(32, saturation, brightness);
@@ -66,7 +67,7 @@ CHSV purple = CHSV(192, saturation, brightness);
 CHSV pink = CHSV(224, saturation, brightness);
 
 int speedDelay = 10;
-int ledPos = 0;
+int ledPos = 5;
 int direction = 1;
 int colorPos = 0;
 int groupSize = 1;
@@ -78,106 +79,324 @@ CHSV color[] = {red, orange, yellow, green, aqua, blue, purple, pink};
 
 //define colors
 
+int delay1 = 30;
+int delay2 = 25;
+int delay3 = 3000;
+int delay4;
+int delay5;
+int delay6;
+
 void setup()
 {
-  // Uncomment/edit one of the following lines for your leds arrangement.
-  // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(strips, NUM_LEDS);
-  // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-  //  FastLED.addLeds<NEOPIXEL, 1>(strips[6], NUM_LEDS);
-  //  FastLED.addLeds<LED_TYPE, 2>(strips, NUM_LEDS);
-  //  FastLED.addLeds<NEOPIXEL, 3>(strips[4], NUM_LEDS);
-  //  FastLED.addLeds<NEOPIXEL, 4>(strips[3], NUM_LEDS);
-  //  FastLED.addLeds<NEOPIXEL, 5>(strips[2], NUM_LEDS);
-  //  FastLED.addLeds<NEOPIXEL, 6>(strips[1], NUM_LEDS);
-  //  FastLED.addLeds<NEOPIXEL, 7>(strips[0], NUM_LEDS);
-  // FastLED.addLeds<APA104, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2811, DATA_PIN, COLOR_ORDER>(strips, NUM_LEDS);
+  FastLED.setBrightness(brightness);
+  //  for (int i = 0; i < NUM_LEDS; i++) {
+  //    strips[i] = CRGB::White;
+  //    FastLED.delay(1);
+  //    strips[i] = CRGB::Black;
+  //    FastLED.delay(1);
+  //  }
 
-  // FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<P9813, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<APA102, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<DOTSTAR, RGB>(leds, NUM_LEDS);
-
-  // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  FastLED.setBrightness(20);
-
-  // black out strib before doing anything to clear previouse program
   for (int i = 0; i < NUM_LEDS; i++)
   {
     strips[i] = CRGB::Black;
   }
+
   FastLED.show();
-  FastLED.delay(1000);
+
+  // RG
+  // delay1 = 7;
+  // for (int i = 0; i < NUM_LEDS; i += 2)
+  // {
+
+  //   strips[i] = CRGB::Green;
+
+  //   strips[i + 1] = CRGB::Red;
+  //   FastLED.delay(delay1);
+  // }
+  pattern1();
 }
 
 void loop()
 {
 
-  if (pattern == 1)
+  pattern1();
+  pattern2();
+  FastLED.delay(5000);
+  pattern3();
+  FastLED.delay(5000);
+  pattern4();
+  FastLED.delay(5000);
+  pattern5();
+  FastLED.delay(5000);
+  pattern6();
+  FastLED.delay(5000);
+  pattern7();
+  FastLED.delay(5000);
+  pattern8();
+  FastLED.delay(5000);
+  pattern9();
+  FastLED.delay(5000);
+  pattern10();
+  FastLED.delay(5000);
+  for (int i = 0; i < 5000; i++)
   {
-
-    // turn all lights to current set color
-    for (int i = 0; i < NUM_LEDS; i++)
-    {
-      strips[i] = CHSV(h, s, v);
-    }
-
-    // show changes
-    FastLED.show();
-
-    //update hue value
-    h = h + 1;
-    if (h >= 255)
-    {
-      h = 0;
-    }
-
-    FastLED.delay(100);
+    twinkle();
   }
-  else if (pattern == 2)
-  {
 
-    // turn all lights to current set color
-    for (int i = ledPos; i < 5; i++)
+  //for(int i = 0; i < NUM_LEDS; i++){
+  //  strips[i] = CHSV(h, s, v);
+  //
+  //
+  //}
+
+  //  pattern 1
+  //  for (int i = 0; i < NUM_LEDS; i++) {
+  //    strips[i] = CRGB::Green;
+  //    FastLED.delay(delay2);
+  //  }
+  //
+  //  for (int i = 0; i < NUM_LEDS; i++) {
+  //    strips[i] = CRGB::Black;
+  //    FastLED.delay(delay1);
+  //  }
+
+  //    int stripEnd = x + 7;
+  //   for (int i = x; i < stripEnd ; i++){
+  //
+  //
+  //    strips[20+i] = CRGB::Green;
+  //    FastLED.show();
+  //  //  strips[20+i] = CRGB::Black;
+  //    FastLED.delay(20);
+  //
+  //
+  //    if(x>99){
+  //      xSpeed *= -1;
+  //    }
+  //    x += xSpeed;
+  //
+  //
+  //
+  //   }
+  //
+  //
+  //
+  FastLED.delay(1);
+}
+
+void pattern1()
+{
+
+  // red green flash
+  int runcount = 20;
+  while (runcount > 0)
+  {
+    delay2 = 500;
+
+    for (int i = 0; i < NUM_LEDS; i += 2)
     {
-      strips[i] = CHSV(h, s, v);
+
+      strips[i] = CRGB::Green;
+      strips[i + 1] = CRGB::Red;
     }
 
-    // show changes
     FastLED.show();
+    FastLED.delay(delay2);
 
-    FastLED.delay(100);
+    for (int i = 0; i < NUM_LEDS; i += 2)
+    {
+
+      strips[i] = CRGB::Red;
+      strips[i + 1] = CRGB::Green;
+    }
+
+    FastLED.show();
+    FastLED.delay(delay2);
+    runcount--;
   }
 }
 
-// for (int i = 0; i < 7 ; i++){
-//
-//
-//  strips[20+i] = CHSV(224, saturation, (brightness - i));
-//  FastLED.show();
-////  strips[20+i] = CRGB::Black;
-//  delay(30);
-//
-//
-//  if(x>99){
-//    xSpeed *= -1;
-//  }
-//  x += xSpeed;
-//
-//
-//
-// }
+void pattern2()
+{
+
+  int runcount = 20;
+  while (runcount > 0)
+  {
+    // red green roll
+
+    delay1 = 7;
+    delay2 = 10;
+    delay3 = 1000;
+
+    for (int i = 0; i < NUM_LEDS; i += 2)
+    {
+
+      strips[i] = CRGB::Green;
+      FastLED.delay(delay1);
+      strips[i + 1] = CRGB::Red;
+      FastLED.delay(delay1);
+    }
+
+    FastLED.delay(delay3);
+
+    for (int i = 0; i < NUM_LEDS; i += 2)
+    {
+
+      strips[i] = CRGB::Red;
+      FastLED.delay(delay1);
+      strips[i + 1] = CRGB::Green;
+      FastLED.delay(delay1);
+    }
+
+    FastLED.delay(delay3);
+    runcount--;
+  }
+}
+
+void pattern3()
+{
+  // instant RG
+  for (int i = 0; i < NUM_LEDS; i += 2)
+  {
+
+    strips[i] = CRGB::Green;
+    strips[i + 1] = CRGB::Red;
+  }
+}
+void pattern4()
+{
+  // transition center out
+  int center = (NUM_LEDS / 2);
+  int x = 1;
+  for (int i = center; i < NUM_LEDS; i += 1)
+  {
+    strips[i] = CRGB::Blue;
+    strips[center - x] = CRGB::Blue;
+    FastLED.delay(500);
+    x += 1;
+  }
+}
+
+void pattern5()
+{
+  // transition center random color
+  int colorIndex = random(8);
+  CHSV c = color[colorIndex];
+  int center = (NUM_LEDS / 2);
+  int x = 1;
+  for (int i = center; i < NUM_LEDS; i += 1)
+  {
+    strips[i] = c;
+    strips[center - x] = c;
+    FastLED.delay(500);
+    x += 1;
+  }
+}
+
+void pattern6()
+{
+  //clear strip
+  // for (int i = 0; i < NUM_LEDS; i += 1)
+  // {
+  //   strips[i] = CRGB::Black;
+  // }
+
+  // FastLED.delay(1);
+  int pos = 0;
+  for (int j = 0; j < NUM_LEDS; j += 10)
+  {
+
+    for (int i = pos; i < pos + 10; i += 1)
+    {
+      strips[i] = CRGB::Blue;
+      FastLED.delay(50);
+    }
+    FastLED.delay(1000);
+    pos += 10;
+  }
+}
+void pattern7()
+{
+  // randomize all lights
+  for (int i = 0; i < NUM_LEDS; i += 1)
+  {
+    strips[i] = CHSV(random(255), saturation, brightness);
+    FastLED.delay(30);
+  }
+}
+void pattern8()
+{
+  // randomize one single light
+  strips[random(NUM_LEDS)] = CHSV(random(255), saturation, brightness);
+  FastLED.delay(50);
+}
+
+void pattern9()
+{
+
+  for (int i = 0; i < NUM_LEDS; i += 1)
+  {
+    strips[i] = CHSV(random(255), saturation, brightness);
+    FastLED.delay(150);
+    // strips[i] = CRGB::Black;
+    // FastLED.delay(30);
+  }
+}
+
+void pattern10()
+{
+
+  delay1 = 150;
+
+  for (int i = 0; i < NUM_LEDS; i += 1)
+  {
+    strips[i] = CHSV(random(255), saturation, brightness);
+    FastLED.delay(delay1);
+    // strips[i] = CRGB::Black;
+    // FastLED.delay(30);
+    int route = random(10);
+    if (route)
+    {
+      delay1 -= random(5);
+    }
+    else
+    {
+      delay1 += random(25);
+    }
+    if (delay1 < 1)
+    {
+      delay1 = 1;
+    }
+  }
+}
+
+void twinkle()
+{
+  int light = random(NUM_LEDS);
+  CRGB backup = strips[light];
+
+  strips[light] = CRGB::White;
+  FastLED.delay(50);
+  strips[light] = backup;
+  FastLED.delay(random(50, 1000));
+}
+
+void breathe()
+{
+  // breath
+  FastLED.setBrightness(brightness);
+  FastLED.delay(1);
+  FastLED.show();
+  brightness += brightnessSpeed;
+  if (brightness > 255)
+  {
+    brightness = 254;
+    brightnessSpeed *= -1;
+  }
+  if (brightness < 70)
+  {
+    brightness = 71;
+    brightnessSpeed *= -1;
+  }
+}
